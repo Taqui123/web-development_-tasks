0acb85d570adf809523a5c4c23e10ec4f1d8c99c
Syed Taqiuddin.R <125341726+Taqui123@users.noreply.github.com>
2023-11-14 12:45:56 +0530

    disney hotstar clone

---
+++ task- 2 hotstar clone/data.js
@@ -0,0 +1,27 @@
+let movies = [
+    {
+        name :'loki',
+        des :'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque beatae, autem quia itaque repudiandae vero hic voluptatum enim sint laudantium?',
+        image : 'slider 1.png'
+    },
+    {
+        name :'falcon and the winter soldier',
+        des :'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque beatae, autem quia itaque repudiandae vero hic voluptatum enim sint laudantium?',
+        image : 'slider 2.png'
+    },
+    {
+        name :'wonda vision',
+        des :'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque beatae, autem quia itaque repudiandae vero hic voluptatum enim sint laudantium?',
+        image : 'slider 3.png'
+    },
+    {
+        name :'raya and the last dragon',
+        des :'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque beatae, autem quia itaque repudiandae vero hic voluptatum enim sint laudantium?',
+        image : 'slider 4.png'
+    },
+    {
+        name :'luca',
+        des :'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque beatae',
+        image : 'slider 5.png'
+    }
+]
